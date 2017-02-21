96143d20d3c313d38941c4fd0113c4611382717b
susanna16 <jahnne@naver.com>
2017-02-21 15:56:53 +0900

    05-for-loop.py -Learn if, else, elif, for, range, while

---
+++ 05-for-loop.py
@@ -0,0 +1,15 @@
+#for, range
+#while
+
+# #for
+# for num in range(1,10):
+#     print(num)
+#
+# num_list=[1,2,3,4,5,6,7,8,9,10]
+# for num in num_list:
+#     print(num)
+
+a=1
+while a<10:
+    print(a)
+    a=a+1
